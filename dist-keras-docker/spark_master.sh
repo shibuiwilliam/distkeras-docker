ed673580ed1676eafc42f34ab145b18dd0c5032f
shibuiwilliam <shibuiyusuke@gmail.com>
2017-04-07 05:44:36 +0900

    changed

--- dist-keras-docker/spark_master.sh
+++ dist-keras-docker/spark_master.sh
@@ -9,4 +9,4 @@ ${SPARK_HOME}/sbin/start-slave.sh -c 1 -m 3G ${MASTER}
 
 export PYSPARK_SUBMIT_ARGS="--master ${MASTER}"
 
-nohup jupyter notebook --NotebookApp.token='' &
+nohup jupyter notebook --NotebookApp.token=''  --allow-root &
