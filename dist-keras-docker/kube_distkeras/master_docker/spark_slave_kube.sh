b27d8698593262c33e3d2cd58b4e114ad57f46b7
shibuiwilliam <shibuiyusuke@gmail.com>
2017-05-07 13:24:25 +0900

    updated

--- dist-keras-docker/kube_distkeras/master_docker/spark_slave_kube.sh
+++ dist-keras-docker/kube_distkeras/master_docker/spark_slave_kube.sh
@@ -5,7 +5,7 @@ MASTER_IP=192.168.10.10
 MASTER_PORT=7077
 MASTER=spark://${MASTER_IP}:${MASTER_PORT}
 
-${SPARK_HOME}/sbin/start-slave.sh -c 1 -m 3G ${MASTER}
+${SPARK_HOME}/sbin/start-slave.sh -c 1 -m 2G ${MASTER}
 
 export PYSPARK_SUBMIT_ARGS="--master ${MASTER}"
 
